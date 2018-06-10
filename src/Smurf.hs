ea11ec6df505be1d502746042ab126cfa5241c74
Zebulun Arendsee <arendsee@iastate.edu>
2018-06-10 18:42:08 -0500

    Removed the last toxic bits of `indents`

--- src/Smurf.hs
+++ src/Smurf.hs
@@ -3,10 +3,9 @@
 module Smurf (parseSmurf) where
 
 import Text.Parsec hiding (State)
-import Text.Parsec.Indent
+import Text.Parsec.String (Parser)
 import Control.Monad.State
 
-type IParser a = IndentParser String () a
 type Smurf = [Statement]
 data Statement
   = Signature Name [InputType] OutputType [Constraint]
@@ -18,30 +17,26 @@ type OutputType = String
 type Constraint = String
 type Name = String
 
-iParse :: IParser a -> SourceName -> String -> Either ParseError a
-iParse aParser source_name input =
-  runIndentParser aParser () source_name input
-
 parseSmurf :: SourceName -> String -> String
 parseSmurf file s =
-  case iParse smurf file s of
+  case parse smurf file s of
     Left  err    -> show err    ++ "\n"
     Right result -> show result ++ "\n"
 
-smurf :: IParser Smurf
+smurf :: Parser Smurf
 smurf = do
   result <- many1 aStatement 
   -- eof
   return result
 
-aStatement :: IParser Statement
+aStatement :: Parser Statement
 aStatement = do
   s <-  try aSignature
     <|> try aSource
     <?> "a Statement"
   return s
 
-aSource :: IParser Statement
+aSource :: Parser Statement
 aSource = do
   spaces
   string "source"
@@ -53,17 +48,17 @@ aSource = do
   source <- many aSourceLine
   return $ Source lang source
 
-aSpace :: IParser Char
+aSpace :: Parser Char
 aSpace = char ' ' <|> char '\t'
 
-aSourceLine :: IParser String
+aSourceLine :: Parser String
 aSourceLine = do
   s <- many1 aSpace
   l <- many (noneOf "\n")
   newline
   return $ (s ++ l)
 
-aSignature :: IParser Statement 
+aSignature :: Parser Statement 
 aSignature = do
   typename <- aName
   string "::"
@@ -75,26 +70,26 @@ aSignature = do
   constraints <- option [] aConstraintList
   return $ Signature typename inputs output constraints
 
-aConstraintList :: IParser [Constraint]
+aConstraintList :: Parser [Constraint]
 aConstraintList = do
   string "where"
   spaces
   xs <- aNameList
   return xs
 
-aName :: IParser Name
+aName :: Parser Name
 aName = do
   s <- many1 alphaNum
   spaces
   return s
 
-aNameList :: IParser [String]
+aNameList :: Parser [String]
 aNameList = do
   x <- aName
   rs <- many aFollowingName
   return (x:rs)
 
-aFollowingName :: IParser Name
+aFollowingName :: Parser Name
 aFollowingName = do
   spaces
   char ','
